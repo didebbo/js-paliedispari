cd77c3b19955b759e9397e87b63bf068ba7d0c15
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-31 17:16:38 +0200

    modificata funzione isPal

--- palindroma/index.js
+++ palindroma/index.js
@@ -2,10 +2,7 @@
 let str;
 
 // Funzione Palindroma
-const isPal = (str) => {
-    if (str === str.split("").reverse().join("")) return true;
-    return false;
-};
+const isPal = (str) => str === str.split("").reverse().join("");
 
 // Stampo il risultato
 (isPal(str = prompt("Inserisci parola")) ? alert("La parola " + str + " è palindroma") : alert("La parola " + str + " non è palindroma"));
