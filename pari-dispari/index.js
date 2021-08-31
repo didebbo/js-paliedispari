501e0d6a81ba26fb1e34c406ee076eb997ea64ff
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-31 15:29:54 +0200

    aggiunto commenti

--- pari-dispari/index.js
+++ pari-dispari/index.js
@@ -20,12 +20,13 @@ const genNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Controllo dispari
+// Controllo se è dispari
 const isOdd = (num) => {
     if (num % 2) return true;
     return false;
 }
 
+// COntrollo se giocatore ha vinto
 const isWinner = (arg1, num) => {
 
     if (isOdd(num) == arg1) return true;
@@ -40,25 +41,31 @@ const main = (arg1) => {
     let sum = 0;
     let log = "";
 
-    console.log(arg1);
     (arg1 ? log += "Hai scelto Dispari" : log += "Hai scelto Pari");
 
+
+    // Chiedo all'utente un numero
     let num1 = askNumber(min, max);
 
     log += "\nHai scelto il numero " + num1;
 
+    // Genero numero casuale
     let num2 = genNumber(min, max);
 
     log += "\nComputer sceglie " + num2;
 
+    // Eseguo la somma
     sum = num1 + num2;
 
     log += "\nTotale " + sum;
 
+    // Controllo se è dispari
     ((isOdd(sum)) ? log += "\nLa somma è dispari" : log += "\nLa somma è pari");
 
+    // COntrollo se giocatore ha vinto
     ((isWinner(arg1, sum)) ? log += "\nHai vinto!" : log += "\nHai perso!");
 
+    // Stampo il risultato
     alert(log);
 
 }
